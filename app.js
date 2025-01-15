b5b867b91ca262c2c0870e3286d9ff8b707fa5d1
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-15 23:15:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const pool = require("./config/db");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// MySql db connection
+pool
+  .query("SELECT 1")
+  .then(() => {
+    console.log("my sql connect");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(port, () => console.log(`Server started at port ${port}`));
